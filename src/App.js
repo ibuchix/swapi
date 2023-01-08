88166f65595f6e21063e4377baccf1b52c33a5f1
ibuchix <80325669+ibuchix@users.noreply.github.com>
2023-01-08 09:34:32 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import People from './components/People';
+import Planets from './components/Planets';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {Container, Dimmer, Loader} from 'semantic-ui-react';
 import './App.css';
 
 function App() {
+  const [people, setPeople]=useState([]);
+  const [planets, setPlanets]=useState([]);
+  const [loading, setLoading] = useState([]);
+
+  useEffect(()=>{
+    async function fetchPeople(){
+      let res = await fetch('https://swapi.dev/api/people/?format=json');
+      let data = await res.json();
+      setPeople(data.results);
+      setLoading(false);
+    }
+
+    async function fetchPlanets(){
+      let res = await fetch('https://swapi.dev/api/planets/?format=json');
+      let data = await res.json();
+      setPlanets(data.results);
+      setLoading(false);
+    }
+
+    fetchPeople();
+    fetchPlanets();
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+      <Navbar/>
+      <Container fluid>
+        {loading ? (
+          <Dimmer active inverted>
+            <Loader inverted>Loading</Loader>
+          </Dimmer>
+        ) : (
+        <Routes>
+          <Route path='/home' element={<Home data={Home}/>}/>
+          <Route path='/people' element={<People data={people}/>}/>
+          <Route path='/planets' element={<Planets data={planets}/>}/>
+      </Routes>
+        )}
+
+      </Container>
+      </Router>
+    </>
   );
 }
 
 export default App;
+
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <p>SWAPI Test Task - Yomi</p>
+    </div>
+  )
+}
+
+export default Home
